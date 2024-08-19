cab71866f9e3c5b82e78477e59e840c293952b58
akak4456 <akak4456@naver.com>
2024-08-19 22:37:14 +0900

    [ADD] Response Code 추가함

--- src/main/java/com/adele/common/ResponseCode.java
+++ src/main/java/com/adele/common/ResponseCode.java
@@ -1,7 +1,7 @@
 package com.adele.common;
 
 public enum ResponseCode {
-    SUCCESS("0");
+    SUCCESS("0"), Login_ERROR("-1"), RESPONSE_FORM_ERROR("-2");
     private final String code;
     ResponseCode(String code) {
         this.code = code;
